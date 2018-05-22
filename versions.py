bd7a7efd558f8b46b7ca2c8ec3ed185696de32a6
ebreton <email>
2018-05-22 22:03:53 +0200

    added script update_release.py to manage version numbers

---
+++ versions.py
@@ -0,0 +1,16 @@
+# flake8: noqa
+# This file is autognerated by post-commit hook
+
+# the release comes from git and should not be modified
+# => read-only
+_release = '0.1.0'
+
+# you can set the next version number manually
+# if you do not, the system will make sure that version > release
+# => read-write, >_release
+_version = '0.1.1-rc'
+
+# the build number will generate conflicts on each PR merge
+# just keep yours every time
+# => read-only
+_build = '5836280bc74ae8129f06952ed86cd637940a13ce'
