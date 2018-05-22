d3f29825c92e206caf37417091f7709d22c8c1f0
ebreton <email>
2018-05-22 22:33:51 +0200

    Prepared release 0.1.1

--- versions.py
+++ versions.py
@@ -8,7 +8,7 @@ _release = '0.1.0-3-gd7eb5f0'
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
 # => read-write, >_release
-_version = '0.1.1-rc'
+_version = '0.1.1'
 
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
