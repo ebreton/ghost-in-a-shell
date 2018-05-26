49cafdd0aeff459e6a2482311d48c9af9107efb5
ebreton <email>
2018-05-26 10:08:42 +0200

    udpated versions.py

--- versions.py
+++ versions.py
@@ -3,14 +3,14 @@
 
 # the release comes from git and should not be modified
 # => read-only
-_release = '0.1.1-16-g5c88f7a'
+_release = '0.1.2-2-gcec557c'
 
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
 # => read-write, >_release
-_version = '0.1.2'
+_version = '0.1.3-rc'
 
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
 # => read-only
-_build = '5c88f7aca8412b75a210184db1da0ec707a67a1c'
+_build = 'cec557c4e1af42e94f61d3a276c82c4caaa15352'
