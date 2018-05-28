35fe059a85b0efb3270c7a2c5ae57c2f8594fc6b
ebreton <email>
2018-05-28 21:49:24 +0200

    Added make gatling for load tests

--- versions.py
+++ versions.py
@@ -3,7 +3,7 @@
 
 # the release comes from git and should not be modified
 # => read-only
-_release = '0.2.0-1-gfa9b278'
+_release = '0.2.0-11-g0620b66'
 
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
@@ -13,4 +13,4 @@ _version = '0.2.1-rc'
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
 # => read-only
-_build = 'fa9b2789fc518d6be015dbff11976fc14eb4584d'
+_build = '0620b6603ddcb0235fa2207671e85f247ac69697'
