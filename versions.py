a77bda6622b03a531520117babe5b02291203e0d
ebreton <email>
2018-06-06 18:26:26 +0200

    added diagram for blog part 3

--- versions.py
+++ versions.py
@@ -3,14 +3,14 @@
 
 # the release comes from git and should not be modified
 # => read-only
-_release = '0.2.1-14-g9142056'
+_release = '0.2.2-3-g1ad7d6b'
 
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
 # => read-write, >_release
-_version = '0.2.2'
+_version = '0.2.3-rc'
 
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
 # => read-only
-_build = '91420562d8c364ba57fc0204d558f431ea2127f2'
+_build = '1ad7d6bf31690646f338150a8d7206d57a0bae37'
